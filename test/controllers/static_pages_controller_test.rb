2e9dfce7116786947a1a1c1bbdeea41b1c039f4f
Angie Green <angie@greenorleans.com>
2015-04-13 14:19:03 -0500

    Add titles to all views

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | 52 Apps Project by Angie Green"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | 52 Apps Project by Angie Green"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | 52 Apps Project by Angie Green"
   end
 
   test "should get categories" do
     get :categories
     assert_response :success
+    assert_select "title", "All 52 Projects | 52 Apps Project by Angie Green"
   end
 
 end
