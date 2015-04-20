eb00adf8dae95d92614f4cab5926f7874afffa0e
Angie Green <angie@greenorleans.com>
2015-04-19 20:00:18 -0500

    Still trying to fix bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,15 +9,33 @@ class StaticPagesController < ApplicationController
   end
 
   def rules
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @categories }
+    end
   end
 
   def about
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @categories }
+    end
   end
 
   def contact
   end
 
   def goals
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @categories }
+    end
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   default_scope -> { order(created_at: :desc) }
   mount_uploader :photo, PhotoUploader
-  validates :category_id, presence: true
   validates :content, presence: true
   validates :day, presence: true
   validates :title, presence: true
-  self.per_page = 10
 end
