8ccc2e5a90bd805b6ca40ff91873e661a6ac2f19
Angie Green <angie@greenorleans.com>
2015-04-23 13:16:15 -0500

    Fix after login path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       remember user
-      redirect_to new_posts_path
+      redirect_to new_category_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
     end
