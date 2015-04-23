b6a8b768a27b5cfd6118ca38d25116a2ec476289
Angie Green <angie@greenorleans.com>
2015-04-23 13:20:20 -0500

    Remove authorize

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  skip_before_action :authorize, only: [:show, :index]
+  skip_before_action :authorize
 
   def index
   @posts = Post.all.paginate(:page => params[:page], :per_page => 10)
