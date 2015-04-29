f3ea7625d20ae9d61e315d43e2b5487b0ef88cd8
Angie Green <angie@greenorleans.com>
2015-04-29 11:56:36 -0500

    Remove carrierwave for now

--- lib/carrierwave/storage/fog.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAJGQPV5OL6DMGSMYA',       # required
-    :aws_secret_access_key  => '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',       # required
-    :region                 => 'us-west-2'  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = '52apps'                     # required
-end
